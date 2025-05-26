b96ee0cbf7ce79302670557b9d65edc456d3e36f
Bruno C. <v3ktoryt@gmail.com>
2025-05-25 23:23:40 -0300

    ✨Feat: Creating the function to read the files and transform them into a vector of lines, and reading all the files expected in our configuration

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,29 @@
 mod config;
 
 use config::Configuration;
-use std::env;
+use std::{
+    env,
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
     let config = Configuration::new(args);
 
-    println!("{:?}", config);
+    for file in &config.file_names {
+        let file_content = read_file(file).expect(&format!("Cound not read file {}", file));
+        println!("File: {} contains: {}", file, file_content.len());
+    }
+
+    //println!("{:?}", config);
+}
+
+fn read_file(file_name: &str) -> Result<Vec<String>, ()> {
+    if let Ok(file) = File::open(file_name) {
+        let reader = BufReader::new(file);
+        Ok(reader.lines().map(|l| l.unwrap_or(String::new())).collect())
+    } else {
+        Err(())
+    }
 }
